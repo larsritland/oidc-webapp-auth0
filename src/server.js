a8e7414d766356d509e20ec92a4ada7cf61ffaf0
Lars Ritland <lars.ritland@gmail.com>
2020-04-10 11:05:38 +0200

    Update src/server.js - changed for using SDKs to Request Delegated Authorization

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ const auth0Strategy = new Auth0Strategy(
   },
   (accessToken, refreshToken, extraParams, profile, done) => {
     profile.idToken = extraParams.id_token;
+    profile.accessToken = extraParams.access_token;
     return done(null, profile);
   }
 );
@@ -65,7 +66,8 @@ app.get('/profile', (req, res) => {
 app.get(
   '/login',
   passport.authenticate('auth0', {
-    scope: 'openid email profile'
+    audience: process.env.API_IDENTIFIER,
+    scope: 'openid email profile read:to-dos'
   })
 );
 
@@ -85,8 +87,26 @@ app.get('/callback', (req, res, next) => {
 });
 
 app.get('/to-dos', async (req, res) => {
-  res.status(501).send();
+  const delegatedRequestOptions = {
+    url: 'http://localhost:3001',
+    headers: {
+      Authorization: `Bearer ${req.session.passport.user.accessToken}`
+    }
+  };
+  try {
+    const delegatedResponse = await request(delegatedRequestOptions);
+    const toDos = JSON.parse(delegatedResponse);
+    res.render('to-dos', {
+      toDos
+    });
+  } catch (error) {
+    res.status(error.statusCode).send(error);
+  }
 });
+
+// app.get('/to-dos', async (req, res) => {
+//   res.status(501).send();
+// });
 
 app.get('/remove-to-do/:id', async (req, res) => {
   res.status(501).send();
